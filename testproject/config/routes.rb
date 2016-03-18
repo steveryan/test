fe258e63fa0051c87b5911a6edca31f659e5450b
steveryan6989 <steveryan6989@gmail.com>
2016-03-18 16:10:53 -0400

    followed tutorial to create job board
    https://medium.com/@riklomas/how-to-create-a-simple-jobs-board-in-ruby-o
    n-rails-even-if-youve-never-coded-before-9b296c4df483#.ll9904kb2

--- testproject/config/routes.rb
+++ testproject/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :jobs
+  root 'jobs#index'
+  get 'jobs/index'
+
+  get 'jobs/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
